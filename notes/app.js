d88ae457bf46b8d2b1379fe51e9c5fc37239ca95
Anthony Eriksen <ajrayid@gmail.com>
2020-03-03 12:06:24 -0700

    Notes, Exercises

--- notes/app.js
+++ notes/app.js
@@ -55,8 +55,8 @@
 // Check if the values in arr2 match the values in arr1^2. Doesn't have to be in order.
 
 // This time complexity is O(n^2) Because of the for loop which is O(n), and the nested indexOf() method which is also O(n).
-const a = [1, 2, 3];
-const b = [4, 1, 9];
+// const a = [1, 2, 3];
+// const b = [4, 1, 9];
 
 // function same(arr1, arr2) {
 //     if (arr1.length !== arr2.length) {
@@ -161,7 +161,7 @@ const b = [4, 1, 9];
 //     return true;
 // }
 
-// console.log(isAnagram("anagram", "agnaram"));
+// console.log(validAnagram("anagram", "agnaram"));
 
 
 // -----------------------------------------
@@ -169,7 +169,7 @@ const b = [4, 1, 9];
 // This is using a multiple pointers pattern. Setting variables has index values, one starts from the left, or start. The other starts from the right or end, and they work towards the middle.
 // This approach removes the need for a nested forLoop which has a time complexity of O(n^2).
 // This approach has a time complexity of O(n). Much much better.
-// My question is, what if in a large array of sorted numbers, the pairs existed on either side of the middle? It wouldn't catch those pairs.
+// My question is, what if in a large array of sorted numbers, the pairs existed on either side of the middle? It wouldn't catch those pairs?
 // Is this an issue? How could you account for that?
 
 // function sumZero(arr) {
@@ -252,8 +252,244 @@ const b = [4, 1, 9];
 // }
 // console.log(maxSubArraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
 
-function dupes(...args) {
-    console.log(args)
-}
 
-dupes('a', 'a', 'b', 'e', 2, 5, 6, 7);
+// Check for Duplicates exercise
+// My Solution
+// --------------
+// function areThereDuplicates(...args) {
+//     let obj = {};
+//     args.forEach(item => {
+//         obj[item] ? obj[item] += 1 : obj[item] = 1;
+//     })
+//     let keys = Object.entries(obj)
+//     let index = 1;
+//     for (let i = 0; i < keys.length; i++) {
+//         if (keys[i][index] >= 2) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// ------------------------------------------------
+// Instructor Solutions
+// Same Frequency
+// function sameFrequency(num1, num2) {
+//     let strNum1 = num1.toString();
+//     let strNum2 = num2.toString();
+//     if (strNum1.length !== strNum2.length) return false;
+
+//     let countNum1 = {};
+//     let countNum2 = {};
+
+//     for (let i = 0; i < strNum1.length; i++) {
+//         countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
+//     }
+
+//     for (let j = 0; j < strNum1.length; j++) {
+//         countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
+//     }
+
+//     for (let key in countNum1) {
+//         if (countNum1[key] !== countNum2[key]) return false;
+//     }
+
+//     return true;
+// }
+
+// ---------------------------------------
+
+// Instructor Solution ( Frequency Counter)
+// ---------------
+// function areThereDuplicates() {
+//     let collection = {}
+//     for (let val in arguments) {
+//         collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+//     }
+//     for (let key in collection) {
+//         if (collection[key] > 1) return true
+//     }
+//     return false;
+// }
+
+// ------------
+// Multiple Pointers Solution
+// function areThereDuplicates(...args) {
+//     // Two pointers
+//     args.sort((a, b) => a > b);
+//     let start = 0;
+//     let next = 1;
+//     while (next < args.length) {
+//         if (args[start] === args[next]) {
+//             return true
+//         }
+//         start++
+//         next++
+//     }
+//     return false
+// }
+
+// One-liner
+// function areThereDuplicates() {
+//     return new Set(arguments).size !== arguments.length;
+// }
+// console.log(areThereDuplicates(1, 4, 6, 7, 1))
+// console.log(areThereDuplicates(1, 2, 3))
+// console.log(areThereDuplicates(1, 2, 2))
+// console.log(areThereDuplicates('a', 'b', 'c', 'a'))
+// console.log(areThereDuplicates('a', 'b', 'f', 'e', 'b'))
+
+
+// ---------------------------------------------------------------------
+
+// // Pair Average using Multiple Pointers
+// function averagePair(arr, num) {
+//     let start = 0
+//     let end = arr.length - 1;
+//     while (start < end) {
+//         let avg = (arr[start] + arr[end]) / 2
+//         if (avg === num) return true;
+//         else if (avg < num) start++
+//         else end--
+//     }
+//     return false;
+// }
+
+// console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 4))
+// averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8);
+
+// // Multiple Pointers - isSubsquence
+
+// function isSubsequence(str1, str2) {
+//     var i = 0;
+//     var j = 0;
+//     if (!str1) return true;
+//     while (j < str2.length) {
+//         if (str2[j] === str1[i]) i++;
+//         if (i === str1.length) return true;
+//         j++;
+//     }
+//     return false;
+// }
+
+// console.log(isSubsquence('hello', 'hello world')) // true
+// console.log(isSubsquence('sing', 'sting')) // true
+// console.log(isSubsquence('abc', 'acb')) // false
+
+// ----------------------------------------------------
+
+// SLIDING WINDOWS
+
+// ---------------------------------------------------
+
+// Max Sub Array Sum
+
+// Given an array of integers and a number, write a function called maxSubArraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+// Time Complexity: O(n)
+// Space Complexity O(1)
+
+// Note that the subarray must consist of consecutive elements from the original array.
+
+// e.g. maxSubbarraySum(  [ 100, 200, 300, 400], 2)  // 700 = 300 + 400
+
+// function maxSubarraySum(arr, num) {
+//     if (arr.length < num) {
+//         return null;
+//     }
+
+//     let total = 0;
+//     for (let i = 0; i < num; i++) {
+//         total += arr[i];
+//     }
+//     let currentTotal = total;
+//     for (let i = num; i < arr.length; i++) {
+//         currentTotal += arr[i] - arr[i - num];
+//         total = Math.max(total, currentTotal);
+//     }
+//     return total;
+// }
+
+// console.log(maxSubarraySum([1, 3, 6, 8, 13, 17], 3))
+
+
+// Min Sub Array Length
+
+// Write a function called minSubArrayLen which accepts two parameters, and array of POSITIVE integers and a POSITIVE integer.
+
+// This function should return the minimal length of a contiguous subarray which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
+
+// Time Complexity: O(n)
+// Space Complexity O(1)
+
+// Examples
+// minSubArrayLen([2,3,1,2,4,3], 7) => 2, because [4,3] is the smallest subarray.
+// minsubArrayLen([2,1,6,5,4], 9) => 2, because [5,4] is the smallest subarray.
+// minsubArrayLen([1,4,16,22,5,7,8,9,10], 39) // 3
+
+
+// function minSubArrayLen(nums, sum) {
+//     let total = 0;
+//     let start = 0;
+//     let end = 0;
+//     let minLen = Infinity;
+
+//     while (start < nums.length) {
+//         // if current window doesn't add up to the given sum then
+//         // move the window to right
+//         if (total < sum && end < nums.length) {
+//             total += nums[end];
+//             end++;
+//         }
+//         // if current window adds up to at least the sum given then
+//         // we can shrink the window
+//         else if (total >= sum) {
+//             minLen = Math.min(minLen, end - start);
+//             total -= nums[start];
+//             start++;
+//         }
+//         // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+//         else {
+//             break;
+//         }
+//     }
+
+//     return minLen === Infinity ? 0 : minLen;
+// }
+// console.log(minSubArrayLen([1, 4, 6, 8, 10, 15, 17, 25], 10));
+
+// -------------------------------------------------------------------------------
+
+// Find the Longest Substring
+
+// Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
+
+// Examples
+// findLongestSubstring(' ') =>  0
+// findLongestSubstring(' thisisawesome') => 6)
+// findLongestSubstring('thecatinthehat') => 7
+// findLongestSubstring('bbbbbb') => 1
+// findLongestSubstring('thisishowwedoit') => 6
+
+// Time Complexity => O(n)
+
+
+// function findLongestSubstring(str) {
+//     let longest = 0;
+//     let seen = {};
+//     let start = 0;
+
+//     for (let i = 0; i < str.length; i++) {
+//         let char = str[i];
+//         if (seen[char]) {
+//             start = Math.max(start, seen[char]);
+//         }
+//         // index - beginning of substring + 1 (to include current in count)
+//         longest = Math.max(longest, i - start + 1);
+//         // store the index of the next char so as to not double count
+//         seen[char] = i + 1;
+//     }
+//     return longest;
+// }
+
+// console.log(findLongestSubstring('Hello, this is a string, where is the longest substring?'))
