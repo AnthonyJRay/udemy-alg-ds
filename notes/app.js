4498447642e0be45f2488281ed821eb3990167f2
Anthony Eriksen <ajrayid@gmail.com>
2020-03-04 22:17:22 -0700

    Notes, Recursion, Code Quiz

--- notes/app.js
+++ notes/app.js
@@ -493,3 +493,212 @@
 // }
 
 // console.log(findLongestSubstring('Hello, this is a string, where is the longest substring?'))
+
+// ----------------------------------------------------
+// ----------------------------------------------------
+
+//          RECURSION SOLUTIONS
+
+// POWER SOLUTION
+
+// function power(base, exponent) {
+//     if (exponent === 0) {
+//         return 1;
+//     }
+//     return base * power(base, exponent - 1);
+// }
+
+// ----------------------------------------------------
+
+// FACTORIAL SOLUTION
+
+// function factorial(x) {
+//     if (x < 0) return 0;
+//     if (x <= 1) return 1;
+//     return x * factorial(x - 1);
+// }
+
+// ----------------------------------------------------
+
+// PRODUCT OF ARRAY SOLUTION
+
+// function productOfArray(arr) {
+//     if (arr.length === 0) {
+//         return 1;
+//     }
+//     return arr[0] * productOfArray(arr.slice(1));
+// }
+
+// ----------------------------------------------------
+
+// RECURSIVE RANGE SOLUTION
+
+// function recursiveRange(x) {
+//     if (x === 0) {
+//         return 0;
+//     }
+//     return x + recursiveRange(x - 1);
+// }
+
+// ----------------------------------------------------
+
+// FIBONACCI SOLUTION
+
+// function fib(n) {
+//     if (n <= 2) {
+//         return 1;
+//     }
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// ----------------------------------------------------
+
+// REVERSE STRING RECURSIVELY
+
+// function reverse(str) {
+//     if (str.length <= 1) {
+//         return str;
+//     }
+//     return reverse(str.slice(1)) + str[0];
+// }
+
+// ----------------------------------------------------
+
+// isPalindrome Solution
+
+// function isPalindrome(str) {
+//     if (str.length === 1) return true;
+//     if (str.length === 2) return str[0] === str[1];
+//     if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1))
+//     return false;
+// }
+
+// ----------------------------------------------------
+
+// someRecursive Solution
+
+// function someRecursive(array, callback) {
+//     if (array.length === 0) return false;
+//     if (callback(array[0])) return true;
+//     return someRecursive(array.slice(1), callback);
+// }
+
+// ----------------------------------------------------
+
+//  FLATTEN SOLUTION
+
+// function flatten(oldArr) {
+//     var newArr = []
+//     for (var i = 0; i < oldArr.length; i++) {
+//         if (Array.isArray(oldArr[i])) {
+//             newArr = newArr.concat(flatten(oldArr[i]))
+//         } else {
+//             newArr.push(oldArr[i])
+//         }
+//     }
+//     return newArr;
+// }
+
+// ----------------------------------------------------
+
+// capitalizeFirst Solution
+
+// function capitalizeFirst(array) {
+//     if (array.length === 1) {
+//         return [array[0][0].toUpperCase() + array[0].substr(1)];
+//     }
+//     const res = capitalizeFirst(array.slice(0, -1));
+//     const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length - 1)[0].substr(1);
+//     res.push(string);
+//     return res;
+// }
+
+// ----------------------------------------------------
+
+// nestedEvenSum Solution
+
+// function nestedEvenSum(obj, sum = 0) {
+//     for (var key in obj) {
+//         if (typeof obj[key] === 'object') {
+//             sum += nestedEvenSum(obj[key]);
+//         } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0) {
+//             sum += obj[key];
+//         }
+//     }
+//     return sum;
+// }
+
+// ----------------------------------------------------
+
+// capitalizeWords Solution
+
+// function capitalizeWords(array) {
+//     if (array.length === 1) {
+//         return [array[0].toUpperCase()];
+//     }
+//     let res = capitalizeWords(array.slice(0, -1));
+//     res.push(array.slice(array.length - 1)[0].toUpperCase());
+//     return res;
+
+// }
+
+// ----------------------------------------------------
+
+// stringifyNumbers Solution
+
+// function stringifyNumbers(obj) {
+//     var newObj = {};
+//     for (var key in obj) {
+//         if (typeof obj[key] === 'number') {
+//             newObj[key] = obj[key].toString();
+//         } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+//             newObj[key] = stringifyNumbers(obj[key]);
+//         } else {
+//             newObj[key] = obj[key];
+//         }
+//     }
+//     return newObj;
+// }
+
+// ----------------------------------------------------
+
+// collectStrings Solution
+// Helper method version
+
+// function collectStrings(obj) {
+//     var stringsArr = [];
+
+//     function gatherStrings(o) {
+//         for (var key in o) {
+//             if (typeof o[key] === 'string') {
+//                 stringsArr.push(o[key]);
+//             } else if (typeof o[key] === 'object') {
+//                 return gatherStrings(o[key]);
+//             }
+//         }
+//     }
+
+//     gatherStrings(obj);
+
+//     return stringsArr;
+// }
+
+// ----------------------------------------------------
+
+// collectStrings Solution
+// recursive version
+
+// function collectStrings(obj) {
+//     var stringsArr = [];
+//     for (var key in obj) {
+//         if (typeof obj[key] === 'string') {
+//             stringsArr.push(obj[key]);
+//         } else if (typeof obj[key] === 'object') {
+//             stringsArr = stringsArr.concat(collectStrings(obj[key]));
+//         }
+//     }
+
+//     return stringsArr;
+// }
+
+// ----------------------------------------------------
